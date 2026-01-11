c9fd587c0832fe272f5ba91be8ffcd92780c5092
Erik Tollerud <erik.tollerud@gmail.com>
2026-01-10 23:02:02 -0500

    a few updates to plot testing

--- plot_testing.py
+++ plot_testing.py
@@ -10,6 +10,9 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file", type=Path)
+parser.add_argument("-m", "--mean-subtract", action='store_true')
+parser.add_argument("-o", "--outliers-sig", default=0, type=float)
+parser.add_argument("-c", "--channels", default='', type=str)
 args = parser.parse_args()
 
 timestamps = []
@@ -31,5 +34,28 @@ with args.input_file.open("r") as f:
             lst.append(float(s))
         vals.append(lst)
 
-plt.plot(np.array(timestamps) - timestamps[0], np.array(vals))
+v = np.array(vals)
+if args.mean_subtract:
+    v = v - v.mean(axis=0)
+
+if args.channels:
+    if args.outliers_sig != 0:
+        raise ValueError('cannot set both outliers and channels')
+
+    msk = np.zeros(v.shape[1],dtype=bool)
+    for chnum in args.channels.split(','):
+        msk[int(chnum.strip())] = True
+
+    v = v[:,msk]
+
+if args.outliers_sig != 0:
+    stds = v.std(axis=0)
+    stdall = v.std()
+    msk = stds > args.outliers_sig*stdall
+    print('selected channels', np.where(msk)[0], stds[msk])
+
+    v = v[:,msk]
+
+
+plt.plot(np.array(timestamps) - timestamps[0], v)
 plt.show()
