9d83b953db1588a68c198d97a2428234580f9599
Erik Tollerud <erik.tollerud@gmail.com>
2026-01-11 01:55:17 -0500

    working spi-driven leds

--- maghand-firmware/src/main.rs
+++ maghand-firmware/src/main.rs
@@ -3,17 +3,24 @@
 
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Level, Output, OutputDrive, Flex, Pull};
-use embassy_nrf::{bind_interrupts, saadc, peripherals, uarte};
-use embassy_nrf::pwm::{Prescaler, SimplePwm, DutyCycle};
-use embassy_time::Instant;
+use embassy_nrf::{bind_interrupts, saadc, peripherals, uarte, pwm, spim};
+use embassy_nrf::pwm::DutyCycle;
+use embassy_time::{Instant, Timer, Duration};
 use {defmt_rtt as _, panic_probe as _};
 
 use heapless::format;
 use palette::{Hsv, Srgb, IntoColor};
 
+use ws2812_spi::Ws2812;
+use smart_leds::SmartLedsWrite;
+
+const N_KEYS: usize = 24;
+const LED_POWERUP_TIME: Duration = Duration::from_millis(1); // this is just a guess - implicitly it's everything connected to vhi
+
 bind_interrupts!(struct Irqs {
     SAADC => saadc::InterruptHandler;
     UARTE0 => uarte::InterruptHandler<peripherals::UARTE0>;
+    SPIM3 => spim::InterruptHandler<peripherals::SPI3>;
 }); 
 
 fn vhi_on(vhi_pin: &mut Flex) {
@@ -25,7 +32,7 @@ fn vhi_off(vhi_pin: &mut Flex) {
     vhi_pin.set_as_input(Pull::None);
 }
 
-fn hsv_on_board_leds(pwm: &mut SimplePwm, h: f32, s: f32, v: f32) {
+fn hsv_on_board_leds(pwm: &mut pwm::SimplePwm, h: f32, s: f32, v: f32) {
     let rgb: Srgb = Hsv::new(h, s, v).into_color();
     let rgb: Srgb<u8> = rgb.into_format();
     
@@ -33,6 +40,17 @@ fn hsv_on_board_leds(pwm: &mut SimplePwm, h: f32, s: f32, v: f32) {
     let g_duty = DutyCycle::normal(rgb.green as u16);
     let b_duty = DutyCycle::normal(rgb.blue as u16);
     pwm.set_all_duties([r_duty, g_duty, b_duty, DutyCycle::normal(0)]);
+}
+
+fn hsv_cycle_ws2812_data(phase: f32, s: f32, v: f32, fracrainbow: f32) -> [smart_leds::RGB8; N_KEYS] {
+    let mut data = [smart_leds::RGB8::default(); N_KEYS];
+    for (i, led) in data.iter_mut().enumerate() {
+        let h = ((i as f32)/ (N_KEYS as f32) * fracrainbow + phase) % 1.0 * 360.;
+        let rgb: Srgb = Hsv::new(h, s, v).into_color();
+        let rgb: Srgb<u8> = rgb.into_format();
+        *led = smart_leds::RGB8::new(rgb.red, rgb.green, rgb.blue);
+    }
+    data
 }
 
 #[embassy_executor::main]
@@ -56,21 +74,32 @@ async fn main(_spawner: Spawner) {
 
     // setup board LEDs using PWM - note these are active *low*, so idle level is high
     let mut pwm_config = embassy_nrf::pwm::SimpleConfig::default();
-    pwm_config.ch0_drive = OutputDrive::HighDrive;
-    pwm_config.ch1_drive = OutputDrive::HighDrive;
-    pwm_config.ch2_drive = OutputDrive::HighDrive;
+    // not much visible difference between standard and high drive on the board LEDs, so use standard to save power
+    pwm_config.ch0_drive = OutputDrive::Standard;
+    pwm_config.ch1_drive = OutputDrive::Standard;
+    pwm_config.ch2_drive = OutputDrive::Standard;
     pwm_config.ch0_idle_level = Level::High;
     pwm_config.ch1_idle_level = Level::High;
     pwm_config.ch2_idle_level = Level::High;
-    pwm_config.prescaler = Prescaler::Div16; // 1 Mhz - TODO: see if if this has any significant power/performance impact
+    pwm_config.prescaler = pwm::Prescaler::Div16; // 1 Mhz - TODO: see if if this has any significant power/performance impact
     pwm_config.max_duty = 255; // compatible with u8 rgb values
 
-    let mut pwm = SimplePwm::new_3ch(p.PWM0, p.P0_26, p.P0_30, p.P0_06, &pwm_config);
+    let mut pwm = pwm::SimplePwm::new_3ch(p.PWM0, p.P0_26, p.P0_30, p.P0_06, &pwm_config);
     pwm.set_all_duties([DutyCycle::normal(255), DutyCycle::normal(0), DutyCycle::normal(0), DutyCycle::normal(0)]); //only red on
     pwm.enable();
 
     // setup DIN for key leds - note only works if vhi is on
-    // p.P0_15
+    let mut spi_config = spim::Config::default();
+    spi_config.frequency = spim::Frequency::M4;
+    let mut keyleds = Ws2812::new(spim::Spim::new_txonly_nosck(p.SPI3, Irqs, p.P0_15, spi_config));
+    // power up vhi for the leds
+    vhi_on(&mut vhi_pin);
+    Timer::after(LED_POWERUP_TIME).await;
+    // blink them for a moment at startup
+    keyleds.write([smart_leds::RGB8::new(1, 0, 1); N_KEYS].iter().cloned()).expect("couldn't blink key leds");
+    Timer::after(Duration::from_millis(100)).await;
+    keyleds.write([smart_leds::RGB8::new(0, 0, 0); N_KEYS].iter().cloned()).expect("couldn't clear key leds");
+    //TODO: compare to using pwm for key leds p.P0_15
 
     // setup accelerometer i2c
     // 6D_PWR: p.P0_08 -> output high to enable accelerometer
@@ -155,5 +184,7 @@ async fn main(_spawner: Spawner) {
         uart.write(format!(23; "{}\r\n", midsample_micros).expect("ts formatting failed").as_bytes()).await.expect("uart couldn't write timestamp");
         
         hsv_on_board_leds(&mut pwm, ((postsample.as_millis() as f32)*(360./10000.)) % 360., 1.0, 1.0); //color cycle in 10 secs
+        let ws2812_data = hsv_cycle_ws2812_data((postsample.as_millis() as f32)/5000., 1.0, 0.015, 0.3); // rotates through 5 seconds
+        keyleds.write(ws2812_data.iter().cloned()).expect("couldn't update key leds");
     }
 }
